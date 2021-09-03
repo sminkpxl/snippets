ef60e060cd7f36890b32bdbbf566069db35ab5cc
Steve Mink <steve@pixelsontarget.com>
2021-09-03 07:59:29 -0400

    add bunch of things: mank, sudo/su/alias, file, psm, lsusb, versioning, hashes, etc.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -18,10 +18,24 @@
 #    netsh wlan show profile name="*" key=clear | Where-Object { $_ -match "SSID name\s*:\s(.*)$"} | ForEach-Object { $matches[1].Replace('"','') } `
 #    ref: https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/understanding-rest-web-services-in-powershell
 #    ref: https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/dumping-wi-fi-passwords
+#    netstat -an | find `"443`" -> works - escape " with `
+
+# actual windows mvp powershell profile - check it out: https://gist.github.com/potatoqualitee/cf3ee4146991da80a8e38feeadb47473
+
+# this to: Get-Alias a* will show all aliases starting with an 'a'
+# kill is already aliased to stop-process
 
 # remove "bad" aliases
 remove-item -path alias:ls
 remove-item -path alias:pwd
+
+# create some vars
+$myprofversion = '1.5'
+
+# Get rid of the ps7 promotion - open powershell with -nologo for cleanest experience
+Clear-Host
+Write-Host "Windows PowerShell"
+Write-Host
 
 # git related
 function gs { git status $args }
@@ -29,18 +43,44 @@ function gd { git diff $args }
 function gsm { git ls-files -m . }
 function gcp { git cherry-pick $args }
 function gs { git status $args }
+function dirty { git describe --tag --long --dirty }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5    { Get-FileHash -Algorithm MD5 $args }
+function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin
+{
+    if ($args.Count -gt 0)
+    {   
+       $argList = "& '" + $args + "'"
+       Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $argList
+    }
+    else
+    {
+       Start-Process "$psHome\powershell.exe" -Verb runAs
+    }
+}
+Set-Alias -Name su -Value admin
+Set-Alias -Name sudo -Value admin
 
 # functions
 function runcmd { cmd /c $args }
 function runbash { bash -c $args }
+function fortune { bash -c fortune }
 function vcal { bash -c "/home/smink/.bin/vcal.sh $args" }
+# TODO: something is up when these 3 are on the right side of a pipe as opposed to either the beginning or without a pipe
 function v { gvim.exe $args }
 function vi { gvim.exe $args }
 function gvim { gvim.exe $args }
 function .. { cd .. }
 function ls { bash -c "ls $args" }
-function version { get-wmiobject -class win32_operatingsystem | select caption } 
-function psversion { (Get-Host).Version } 
+function version { echo $myprofversion; get-wmiobject -class win32_operatingsystem | select caption } 
+function psversion { echo(Get-Host).Version } 
 function calc { bash -c "echo $args" }
 function fh { bash -c "find * -type f | xargs grep -HI $args" }
 function fhi { bash -c "find * -type f | xargs grep -HIi $args" }
@@ -63,6 +103,21 @@ function touch
 	    set-content -Path $args[0] -Value $null
 	}
 }
+function lsusb { gwmi Win32_USBControllerDevice }
+function psm 
+{
+    if ($args.Count -lt 1)
+	{
+	    get-process | select id, ProcessName
+	}
+	else
+	{
+		# TODO: this does not work
+	    get-process | select id, ProcessName | where {$_.processname -like $args[0]}
+	}
+}
+function mank { get-help $args[0] | select name, category, synopsis | ft -a }
+function file { bash -c "file ${args}" }
 #alias gsmr='for m in $(git ls-files -m); do test -z "$(git diff -w $m)" || echo $m;done'
 #alias reposrc='[[ -f .git/config ]] && grep url .git/config | cut -d= -f2 | xargs || echo not a valid git repository'
 #alias runflake='flake8 --max-complexity 12 --ignore E303,W391'
@@ -104,3 +159,10 @@ Set-Alias -Name npp -Value "C:\Program Files\Notepad++\notepad++.exe"
 #     2. reload the current profile: & $profile
 #     3. Get version of various things: $PSVersionTable
 #     4. Get Powershell version only: Get-Host | Select-Object Version
+
+#New-Alias -name locate -value C:\Users\Steve Mink\AppData\Local\locate\Invoke-Locate.ps1 -scope Global -force
+#New-Alias -name updatedb -value C:\Users\Steve Mink\AppData\Local\locate\Update-LocateDB.ps1 -scope Global -force
+#New-Alias -name locate -value C:\Users\Steve Mink\AppData\Local\locate\Invoke-Locate.ps1 -scope Global -force
+#New-Alias -name updatedb -value C:\Users\Steve Mink\AppData\Local\locate\Update-LocateDB.ps1 -scope Global -force
+#New-Alias -name locate -value C:\Users\Steve Mink\AppData\Local\locate\Invoke-Locate.ps1 -scope Global -force
+#New-Alias -name updatedb -value C:\Users\Steve Mink\AppData\Local\locate\Update-LocateDB.ps1 -scope Global -force
